25661f0b3c59bb4fc4a96651f2c30b91662c5d46
SHYAMGOPAL BISWAS <shyamgopalbiswas22114@gmail.com>
2024-03-04 23:26:19 +0530

    services added

---
+++ React-Routing-Navbar/src/Pages/Services.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Services() {
+    return (
+        <div>
+            <h1 className="products_page"> Welcome to Services Page</h1>
+        </div>
+    )
+}
