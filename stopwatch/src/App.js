341ea8feb1609932b4d8a69d3fd6402501edabd1
SHYAMGOPAL BISWAS <shyamgopalbiswas22114@gmail.com>
2024-03-12 23:08:40 +0530

    Added Stopwatch

--- stopwatch/src/App.js
+++ stopwatch/src/App.js
@@ -1,13 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Stopwatch from './component/Stopwatch';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        
-      </header>
+      <h1>Stopwatch</h1>
+      <Stopwatch />
     </div>
   );
 }
--- stopwatch/src/component/Stopwatch.jsx
+++ stopwatch/src/component/Stopwatch.jsx
@@ -1,34 +1,56 @@
-import React,{useState,useEffect,useRef} from "react";
+import React,{useState,useEffect} from "react";
 
-function Stopwatch(){
-    const[isRunning, setIsRunning]  = useState(false);
+export default function Stopwatch(){
+    const[start, setStart]  = useState(false);
+    const[stop, setStop] = useState(false);
     const[time, setTime] = useState(0);
-    const timerId = useRef(null);
-    const  startRef = useRef(0);
+    const[timer,setTimer] = useState('')
 
     useEffect(() => {
+         let timer = null
+         if(start && !stop){
+            timer = setInterval( ()=>{
+                 setTime(time => time + 1)
+            },1000)
+         }
+         else if(stop){
+           clearInterval(timer)
+         }
+         return ()=>{
+            clearInterval(timer)
+         }
+    },[start,stop]);
 
-    },[isRunning]);
+    useEffect(()=>{
+        const date = new Date(0)
+        date.setSeconds(time)
 
-    function start(){
-        setIsRunning(true);
+        setTimer(date.toISOString().slice(11,19))
+    })
+
+    const handleStart =()=>{
+        setStop(false)
+        setStart(true)
     }
 
-    function stop(){
-        
+    const handleStop =()=>{
+      setStart(false)
+      setStop(true)
     }
-
-    function reset(){
-        
+    
+    const handleReset =()=>{
+         setTime(0)
+         setStart(false)
+         setStop(false)
     }
 
     return(
 
         <div>
-            <div>00:00:00</div>
-            <button onClick={start}>Statrt</button>
-            <button onClick={stop}>Stop</button>
-            <button onClick={reset}>Reset</button>
+            <div>{timer}</div>
+            <button onClick={handleStart}>Start</button>
+            <button onClick={handleStop}>Stop</button>
+            <button onClick={handleReset}>Reset</button>
         </div>
     )
 }
