d5933a2342b27f25144d837907f6243456780087
SHYAMGOPAL BISWAS <shyamgopalbiswas22114@gmail.com>
2024-03-30 22:38:47 +0530

    added component

--- master/src/App.js
+++ master/src/App.js
@@ -24,7 +24,6 @@ function App() {
       {/* <Navbar /> */}
      {/* <FormData /> */}
 
-
      <Firstname.Provider value={"Shyam"}>
        <Lastname.Provider value={"Gopal"}>
           <CompA />
